d62a281e4c676e6be5cad32d261d8873468a4089
Daniel Butler <dabutler89@gmail.com>
2023-02-19 17:26:10 -0500

    More updates

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -35,7 +35,7 @@ export default function ProjectsIndex({ projects }) {
           className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3"
         >
           {projects.map((project) => (
-            <Card as="li" key={project.name}>
+            <Card as="li" key={project.title}>
               <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
                 <Image
                   src={project.logo}
@@ -48,12 +48,12 @@ export default function ProjectsIndex({ projects }) {
                 <Card.Link href={`/projects/${project.slug}`}>{project.platform}</Card.Link>
               </h5>
               <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
-                <Card.Link href={`/projects/${project.slug}`}>{project.name}</Card.Link>
+                <Card.Link href={`/projects/${project.slug}`}>{project.title}</Card.Link>
               </h2>
               <Card.Description>{project.description}</Card.Description>
               <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
                 <LinkIcon className="h-6 w-6 flex-none" />
-                <span className="ml-2">{project.link.label}</span>
+                <span className="ml-2">{project.name}</span>
               </p>
             </Card>
           ))}
