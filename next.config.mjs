742651d16e725f880f5ffba65c7209b3661414a2
Daniel Butler <dabutler89@gmail.com>
2023-02-21 07:34:22 -0500

    Image exporter configs

--- next.config.mjs
+++ next.config.mjs
@@ -7,12 +7,31 @@ const nextConfig = {
   pageExtensions: ['jsx', 'mdx'],
   reactStrictMode: true,
   swcMinify: true,
+  transpilePackages: ["next-image-export-optimizer"],
+  env: {
+    nextImageExportOptimizer_imageFolderPath: "public/images",
+    nextImageExportOptimizer_exportFolderPath: "out",
+    nextImageExportOptimizer_quality: 75,
+    nextImageExportOptimizer_storePicturesInWEBP: true,
+    nextImageExportOptimizer_exportFolderName: "nextImageExportOptimizer",
+
+    // If you do not want to use blurry placeholder images, then you can set
+    // nextImageExportOptimizer_generateAndUseBlurImages to false and pass
+    // `placeholder="empty"` to all <ExportedImage> components.
+    //
+    // If nextImageExportOptimizer_generateAndUseBlurImages is false and you
+    // forget to set `placeholder="empty"`, you'll see 404 errors for the missing
+    // placeholder images in the console.
+    nextImageExportOptimizer_generateAndUseBlurImages: true,
+  },
   experimental: {
     newNextLinkBehavior: true,
     scrollRestoration: true,
   },
   images: {
-    unoptimized: true,
+    loader: "custom",
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
   webpack5: true,
   webpack: (config) => {
