4b5f725fca361b5a0d744f409966fd762dbd4176
Daniel Butler <dabutler89@gmail.com>
2023-02-21 07:38:50 -0500

    Convert to exported image

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,4 +1,4 @@
-import Image from 'next/future/image'
+import ExportedImage from "next-image-export-optimizer";
 import Head from 'next/head'
 import Link from 'next/link'
 import clsx from 'clsx'
@@ -50,7 +50,7 @@ export default function About() {
         <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
           <div className="lg:pl-20">
             <div className="max-w-xs px-2.5 lg:max-w-none">
-              <Image
+              <ExportedImage
                 src={portraitImage}
                 alt=""
                 sizes="(min-width: 1024px) 32rem, 20rem"
