558dfc999cedb45362955c1fa568ff5a65dbf9a2
Daniel Butler <dabutler89@gmail.com>
2023-02-25 16:45:48 -0500

    Better phrasing

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -95,7 +95,7 @@ function Article({ article }) {
         {formatDate(article.date)}
       </Card.Eyebrow>
       <Card.Description>{article.description}</Card.Description>
-      <Card.Cta>Read article</Card.Cta>
+      <Card.Cta>Read more</Card.Cta>
     </Card>
   )
 }
