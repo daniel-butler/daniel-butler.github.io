68d67cb5a1733dbd6552b67abb186cc345d7d06d
Daniel Butler <dabutler89@gmail.com>
2023-03-02 22:37:45 -0500

    Update order on larger screens

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -282,7 +282,7 @@ export default function Home({ articles }) {
       <Photos />
       <Container className="mt-24 md:mt-28">
         <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
-          <div className="space-y-10 lg:pl-16 xl:pl-24">
+          <div className="space-y-10 md:order-last lg:pl-16 xl:pl-24">
             {/*<Newsletter />*/}
             <Resume />
           </div>
