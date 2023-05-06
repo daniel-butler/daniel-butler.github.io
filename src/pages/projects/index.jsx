a1af2f43ebc46db6312f40f22f4ef39dc9636f28
Daniel Butler <dabutler89@gmail.com>
2023-05-05 20:46:32 -0400

    Clean up the space in the cards

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -51,10 +51,6 @@ export default function ProjectsIndex({ projects }) {
                 <Card.Link href={`/projects/${project.slug}`}>{project.title}</Card.Link>
               </h2>
               <Card.Description>{project.description}</Card.Description>
-              <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
-                <LinkIcon className="h-6 w-6 flex-none" />
-                <span className="ml-2">{project.name}</span>
-              </p>
             </Card>
           ))}
         </ul>
